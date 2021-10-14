7155c1a0a92aaaae7b0c414e46fdda351664d31d
Shahana Kabir <shahana.kabir@gmail.com>
2021-10-14 14:30:11 -0700

    Slot machine exercie

---
+++ src/Machine.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+class Machine extends React.Component{
+    render(){
+        const{s1, s2, s3} = this.props;
+        const winner = ((s1 === s2) && (s2 === s3));
+        
+        return (<div>
+            
+           <p>
+               {s1}
+               {s2}
+               {s3}
+               
+           </p>
+        <p>{winner ? 'winner': 'looser'}</p>
+                 
+            
+        </div>
+        )
+    }
+}
+
+export default Machine;
